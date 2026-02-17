9c270ecb34045e31a82b6856a66fd052e8d18fa3
sidabdullah07 <sidabdullah07@gmail.com>
2026-02-17 09:39:54 +0530

    Add files via upload

---
+++ exp__1.py
@@ -0,0 +1,7 @@
+name = input("ENTER YOUR NAME: ")
+uin = input("ENTER YOUR UIN: ")
+marks = input("ENTER YOUR MARKS: ")
+
+print("HELLO ",name)
+print(f"YOUR UIN IS {uin} ")
+print(f"YOU SCORED {marks}%")
