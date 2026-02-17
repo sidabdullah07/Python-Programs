e986a739f60d14dcc54e8d2c7aa49ffbc5307d4d
sidabdullah07 <sidabdullah07@gmail.com>
2026-02-17 09:45:15 +0530

    Add files via upload

---
+++ exp__2.py
@@ -0,0 +1,7 @@
+r= int(input("ENTER RADIUS IN cm: "))
+area = 3.14*r*r
+print(f"AREA OF CIRCLE IS {area}cm^2")
+l= int(input("ENTER LENGTH OF RECTANGLE IN cm: "))
+w= int(input("ENTER WIDTH OF RECTANGLE IN cm: "))
+area2 = l*w
+print(f"AREA OF RECTANGLE IS {area2}cm^2")
