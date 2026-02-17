e744f53ae8eac6b37fe7d340ae64be520daa7d75
sidabdullah07 <sidabdullah07@gmail.com>
2026-02-17 09:49:21 +0530

    Add files via upload

---
+++ exp__4.py
@@ -0,0 +1,6 @@
+Principle_amount = float(input("ENTER THE PRINCIPLE AMOUNT: "))
+Rate_of_interest = float(input("ENTER THE RATE OF INTEREST IN % PER YEAR: "))
+Time = float(input("ENTER TIME IN YEARS: "))
+SI = (Principle_amount*Rate_of_interest*Time)/2
+
+print(f"SIMPLE INTEREST = {SI} ")
