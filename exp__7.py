6457e881d45a189fe99c4a7c39aee94bf3111a7c
sidabdullah07 <sidabdullah07@gmail.com>
2026-02-17 10:26:27 +0530

    Add files via upload

---
+++ exp__7.py
@@ -0,0 +1,8 @@
+print("UIN : 251A010", "DATE : 09-02-2026")
+d={}
+n = int(input("ENTER NUMBER OF STUDENTS : "))
+for i in range(1,n+1):
+  Name = input("NAME : ")
+  Attendance = input("ATTENDANCE : ")
+  d[i] = {"NAME":Name, "ATTENDANCE":Attendance}
+print(d)
