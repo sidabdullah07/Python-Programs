12080de751f56908a27e61c0ef1fc0adafeed9d6
sidabdullah07 <sidabdullah07@gmail.com>
2026-02-19 11:55:09 +0530

    Add files via upload

---
+++ exp__8.py
@@ -0,0 +1,10 @@
+print("UIN : 251A010", "DATE : 09-02-2026")
+s = int(input("Enter size of square: "))
+
+for i in range(s):
+    print("* "*s)
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(1, rows+1):
+    print("* "*i)
