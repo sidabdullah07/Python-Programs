4ecc9f977e5468509f69da031e7c9492a5b66a78
sidabdullah07 <sidabdullah07@gmail.com>
2026-02-19 12:08:15 +0530

    Add files via upload

---
+++ exp__12.py
@@ -0,0 +1,14 @@
+#file = open("s.txt","w")
+#file.write("HELLO WORLD")
+#file.close()
+#file = open("s.txt","a")
+#file.write("HELLO UNIVERSE")
+#file.close()
+file = open("s.txt","r")
+content = file.read().split()
+print(content)
+n = int(input("ENTER THE WORD LENGTH : "))
+for i in content:
+   if len(i)==n:
+       print(i)
+file.close()
