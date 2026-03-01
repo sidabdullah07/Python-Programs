2a0a7e502ed71e94e6dab10d7276a1a35d863528
sidabdullah07 <sidabdullah07@gmail.com>
2026-03-01 15:18:27 +0530

    Update exp__4.py

--- exp__4.py
+++ exp__4.py
@@ -1,6 +1,9 @@
+https://github.com/sidabdullah07/Python-Programs
+print("UIN : 251A010", "DATE : 09-02-2026")
 Principle_amount = float(input("ENTER THE PRINCIPLE AMOUNT: "))
 Rate_of_interest = float(input("ENTER THE RATE OF INTEREST IN % PER YEAR: "))
 Time = float(input("ENTER TIME IN YEARS: "))
 SI = (Principle_amount*Rate_of_interest*Time)/2
 
 print(f"SIMPLE INTEREST = {SI} ")
+
