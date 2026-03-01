157bf2f84471414f45f36f11a90d8ba9a1a6e5e3
sidabdullah07 <sidabdullah07@gmail.com>
2026-03-01 15:26:32 +0530

    Update exp__9.py

--- exp__9.py
+++ exp__9.py
@@ -1,4 +1,4 @@
-
+https://github.com/sidabdullah07/Python-Programs
 #FACTORIAL GENERATOR
 print("UIN : 251A010  DATE : 10-02-2026")
 n = int(input("ENTER NUMBER : "))
@@ -6,3 +6,4 @@ fact = 1
 for i in range(1, n+1):
   fact*=i
 print("FACTORIAL = ", fact)
+
