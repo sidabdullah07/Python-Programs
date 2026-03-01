ce991e5073741b36c6d64dd45c7214726095ab0b
sidabdullah07 <sidabdullah07@gmail.com>
2026-03-01 15:11:27 +0530

    Update exp__2.py

--- exp__2.py
+++ exp__2.py
@@ -1,5 +1,5 @@
 https://github.com/sidabdullah07/Python-Programs
-print("UIN : 251A053", "DATE : 20-01-2026")
+print("UIN : 251A010", "DATE : 20-01-2026")
 r= int(input("ENTER RADIUS IN cm: "))
 area = 3.14*r*r
 print(f"AREA OF CIRCLE IS {area}cm^2")
@@ -7,4 +7,5 @@ l= int(input("ENTER LENGTH OF RECTANGLE IN cm: "))
 w= int(input("ENTER WIDTH OF RECTANGLE IN cm: "))
 area2 = l*w
 print(f"AREA OF RECTANGLE IS {area2}cm^2")
+
 
