abce9ca5565eecb78456c5de4552240dce5319cd
sidabdullah07 <sidabdullah07@gmail.com>
2026-03-01 15:16:56 +0530

    Update exp__14.py

--- exp__14.py
+++ exp__14.py
@@ -1,3 +1,5 @@
+https://github.com/sidabdullah07/Python-Programs
+print("UIN : 251A010", "DATE : 09-02-2026")
 try:
     n1 = int(input("ENTER FIRST VALUE : "))
     n2 = int(input("ENTER SECOND VALUE : "))
@@ -10,3 +12,4 @@ else:
     print("DIVICION : ",d)
 finally:
     print("THANK YOU......")
+
