34d730ed6efbf75842314f209effb6e8cc2b5f65
sidabdullah07 <sidabdullah07@gmail.com>
2026-03-01 15:13:35 +0530

    Update exp__3.py

--- exp__3.py
+++ exp__3.py
@@ -1,3 +1,5 @@
+https://github.com/sidabdullah07/Python-Programs
+print("UIN : 251A010", "DATE : 27-01-2026")
 basic_salary = int(input("ENTER THE BASIC SALARY: "))
 DA = basic_salary*0.70
 TA = basic_salary*0.30
@@ -8,4 +10,5 @@ print(f"DEARNESS ALLOWANCE IS {DA} ")
 print(f"TRAVEL ALLOWANCE IS {TA} ")
 print(f"HOUSE RENT ALLOWANCE {HRA} ")
 print(f"GROSS SALARY IS : {gross_salary} ")
+
 
