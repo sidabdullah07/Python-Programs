64351602b3a381872132f2d3df65cdebeb1332fb
sidabdullah07 <sidabdullah07@gmail.com>
2026-03-01 15:04:06 +0530

    Update exp__1.py

--- exp__1.py
+++ exp__1.py
@@ -1,3 +1,5 @@
+https://github.com/sidabdullah07/Python-Programs
+print("UIN : 251A010", "DATE : 20-01-2026")
 name = input("ENTER YOUR NAME: ")
 uin = input("ENTER YOUR UIN: ")
 marks = input("ENTER YOUR MARKS: ")
@@ -5,3 +7,4 @@ marks = input("ENTER YOUR MARKS: ")
 print("HELLO ",name)
 print(f"YOUR UIN IS {uin} ")
 print(f"YOU SCORED {marks}%")
+
