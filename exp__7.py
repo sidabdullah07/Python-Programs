cfd7e869e8610b53a07e6ad508f5710ae59f9824
sidabdullah07 <sidabdullah07@gmail.com>
2026-03-01 15:23:31 +0530

    Update exp__7.py

--- exp__7.py
+++ exp__7.py
@@ -1,3 +1,4 @@
+https://github.com/sidabdullah07/Python-Programs
 print("UIN : 251A010", "DATE : 09-02-2026")
 d={}
 n = int(input("ENTER NUMBER OF STUDENTS : "))
@@ -6,3 +7,4 @@ for i in range(1,n+1):
   Attendance = input("ATTENDANCE : ")
   d[i] = {"NAME":Name, "ATTENDANCE":Attendance}
 print(d)
+
